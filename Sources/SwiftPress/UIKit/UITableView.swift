9c8afcc85678e35399d4cfd9261b74da264cde28
Muhammed Yusa Sarisoy <yusasarisoy@icloud.com>
2023-10-01 11:05:21 +0200

    Add dequeueReusableCell for UITableViews

--- Sources/SwiftPress/UIKit/UITableView.swift
+++ Sources/SwiftPress/UIKit/UITableView.swift
@@ -1,6 +1,48 @@
 import UIKit
 
 public extension UITableView {
+  /// Dequeues a reusable cell of the specified type using the provided identifier for the given index path.
+  ///
+  /// - Parameters:
+  ///   - identifier: A unique string identifier for the cell to dequeue.
+  ///   - indexPath: The index path specifying the location of the cell in the table view.
+  /// - Returns: An instance of the specified cell type, casted from the dequeued cell.
+  ///
+  /// This method safely dequeues a cell of the specified type using the given identifier and index path.
+  /// If the dequeue operation is successful and the cell can be cast to the specified type, it returns
+  /// the cell. If the dequeue or casting fails, it raises a fatal error.
+  ///
+  /// Example:
+  ///
+  /// ```
+  /// let cell = tableView.dequeueReusableCell(
+  ///   withIdentifier: "YourCellIdentifier",
+  ///   for: indexPath
+  /// )
+  /// // Use 'cell' safely.
+  /// ```
+  ///
+  /// - Note: Make sure that the identifier used matches the one set for the cell in the storyboard or
+  ///   registered with the table view.
+  ///
+  /// - Warning: If the cast to the specified cell type fails, a fatal error will be triggered. Ensure
+  ///   that the identifier and cell type are correctly specified to avoid crashes.
+  ///
+  /// - SeeAlso: `UITableViewCell`
+  func dequeueReusableCell<Element: UITableViewCell>(
+    withIdentifier identifier: String,
+    for indexPath: IndexPath
+  ) -> Element {
+    guard let cell = dequeueReusableCell(
+      withIdentifier: identifier,
+      for: indexPath
+    ) as? Element else {
+      fatalError("Failed to dequeue cell with identifier: \(identifier).")
+    }
+
+    return cell
+  }
+
   /// Register a UITableViewCell class with the table view.
   ///
   /// - Parameter cellClass: The UITableViewCell subclass to register.
--- Tests/SwiftPressTests/UIKit/UITableViewTests.swift
+++ Tests/SwiftPressTests/UIKit/UITableViewTests.swift
@@ -4,30 +4,50 @@ import UIKit
 
 final class UITableViewTests: XCTestCase {
 
+  // MARK: - Properties
+
+  var tableView: UITableView!
+
   // MARK: - Test Setup
 
   override func setUp() {
     super.setUp()
+
+    tableView = UITableView(frame: .zero)
   }
 
   override func tearDown() {
+    tableView = nil
+
     super.tearDown()
   }
 
   // MARK: - Tests
 
+  // MARK: - dequeueReusableCell(withIdentifier:for:)
+
+  func test_whenDequeueReusableCell_expectCellIsNotNil() {
+    // Given
+    tableView.registerCell(YourCustomCell.self)
+
+    let indexPath = IndexPath(item: .zero, section: .zero)
+
+    // When
+    let cell: UITableViewCell = tableView.dequeueReusableCell(
+      withIdentifier: YourCustomCell.identifier,
+      for: indexPath
+    )
+
+    // Then
+    XCTAssertNotNil(cell)
+  }
+
   // MARK: - registerCell(_:)
 
   func test_whenRegisterCell_expectCellIsNotNil() {
-    // Given
-    final class YourCustomCell: UITableViewCell { }
-
-    let tableView = UITableView(frame: .zero)
-
     // When
     tableView.registerCell(YourCustomCell.self)
 
-    // Then
     let cell = tableView.dequeueReusableCell(
       withIdentifier: YourCustomCell.identifier,
       for: IndexPath(
@@ -36,7 +56,12 @@ final class UITableViewTests: XCTestCase {
       )
     )
 
+    // Then
     XCTAssertNotNil(cell)
     XCTAssertTrue(cell is YourCustomCell)
   }
 }
+
+// MARK: - YourCustomCell
+
+final class YourCustomCell: UITableViewCell { }

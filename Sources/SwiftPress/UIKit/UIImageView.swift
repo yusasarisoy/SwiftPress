fd0ecc51ccee2cdbf57013654f4be8083ed0ed53
Muhammed Yusa Sarisoy <yusasarisoy@icloud.com>
2023-10-27 12:58:21 +0300

    Add circularImageView method for UIImageViews

--- Sources/SwiftPress/UIKit/UIImageView.swift
+++ Sources/SwiftPress/UIKit/UIImageView.swift
@@ -27,4 +27,37 @@ public extension UIImageView {
     imageView.contentMode = contentMode
     return imageView
   }
+
+  /// Creates a custom UIImageView with a circular shape and a border.
+  ///
+  /// - Parameters:
+  ///   - image: The image to display in the image view.
+  ///   - borderWidth: The width of the border (default is 2).
+  ///   - borderColor: The color of the border (default is system gray).
+  ///
+  /// Example:
+  ///
+  /// ```swift
+  /// let circularImage = UIImageView.circularImageView(
+  ///   image: UIImage(named: "profile_image") ?? UIImage(),
+  ///   borderWidth: 3,
+  ///   borderColor: .blue
+  /// )
+  /// ```
+  ///
+  /// - Returns: A customized UIImageView instance with a circular shape and a border.
+  ///
+  static func circularImageView(
+    image: UIImage,
+    borderWidth: CGFloat = 2,
+    borderColor: UIColor = .systemGray
+  ) -> UIImageView {
+    let imageView = UIImageView(image: image)
+    imageView.contentMode = .scaleAspectFill
+    imageView.clipsToBounds = true
+    imageView.layer.cornerRadius = imageView.frame.size.width / 2
+    imageView.layer.borderWidth = borderWidth
+    imageView.layer.borderColor = borderColor.cgColor
+    return imageView
+  }
 }
--- Tests/SwiftPressTests/UIKit/UIImageViewTests.swift
+++ Tests/SwiftPressTests/UIKit/UIImageViewTests.swift
@@ -28,4 +28,25 @@ final class UIImageViewTests: XCTestCase {
     XCTAssertEqual((customImageView.accessibilityIdentifier).orEmpty, "exampleImage")
     XCTAssertEqual(customImageView.contentMode, .center)
   }
+
+  // MARK: - circularImageView(image:borderWidth:borderColor:)
+
+  func test_whenCreatingCircularImageView_expectPropertiesAreCorrect() {
+    // Given
+    let image = UIImage(systemName: "person.fill") ?? UIImage()
+
+    // When
+    let circularImageView = UIImageView.circularImageView(
+      image: image, 
+      borderWidth: 3,
+      borderColor: .blue
+    )
+
+    // Then
+    XCTAssertEqual(circularImageView.contentMode, .scaleAspectFill)
+    XCTAssertEqual(circularImageView.clipsToBounds, true)
+    XCTAssertEqual(circularImageView.layer.cornerRadius, circularImageView.frame.size.width / 2)
+    XCTAssertEqual(circularImageView.layer.borderWidth, 3)
+    XCTAssertEqual(circularImageView.layer.borderColor, UIColor.blue.cgColor)
+  }
 }

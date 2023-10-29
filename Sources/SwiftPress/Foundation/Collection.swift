209843063e71589a2d2dedacbb94653491dfdf68
Muhammed Yusa Sarisoy <yusasarisoy@icloud.com>
2023-10-29 12:33:40 +0300

    Add isNilOrEmpty method for Optional Collections

--- Sources/SwiftPress/Foundation/Collection.swift
+++ Sources/SwiftPress/Foundation/Collection.swift
@@ -25,3 +25,41 @@ public extension Collection where Element: Equatable {
     contains { $0 == element }
   }
 }
+
+public extension Optional where Wrapped: Collection {
+  /// Checks if the collection is either nil or empty.
+  ///
+  /// - Returns: `true` if the collection is nil or empty; otherwise, `false`.
+  ///
+  /// This extension property allows you to easily check if a collection is either nil or empty.
+  ///
+  /// Example:
+  ///
+  /// ```swift
+  /// let emptyArray: [Int]? = []
+  /// let nonEmptyArray: [String]? = ["apple", "banana"]
+  /// let nilArray: [Double]? = nil
+  ///
+  /// if emptyArray.isNilOrEmpty {
+  ///     print("The array is nil or empty.")
+  /// } else {
+  ///     print("The array is not nil or empty.")
+  /// }
+  ///
+  /// if nonEmptyArray.isNilOrEmpty {
+  ///     print("The array is nil or empty.")
+  /// } else {
+  ///     print("The array is not nil or empty.")
+  /// }
+  ///
+  /// if nilArray.isNilOrEmpty {
+  ///     print("The array is nil or empty.")
+  /// } else {
+  ///     print("The array is not nil or empty.")
+  /// }
+  /// ```
+  ///
+  var isNilOrEmpty: Bool {
+    (self?.isEmpty).or(true)
+  }
+}

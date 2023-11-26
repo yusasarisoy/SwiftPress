d1fdca067ddbbc59c16862a542b76f1bd189cfd3
Muhammed Yusa Sarisoy <yusasarisoy@icloud.com>
2023-11-26 12:21:57 +0100

    Add appendingQueryParameters method for URLs

--- Sources/SwiftPress/Foundation/URL.swift
+++ Sources/SwiftPress/Foundation/URL.swift
@@ -1,5 +1,43 @@
 import Foundation
 
+public extension URL {
+  /// Appends query parameters to the URL.
+  ///
+  /// This method takes a dictionary of key-value pairs representing query parameters and appends them to the existing URL.
+  ///
+  /// - Parameter parameters: A dictionary containing the query parameters.
+  ///   - Each key-value pair in the dictionary corresponds to a query parameter, where the key is the parameter name, and the value is its associated value.
+  ///
+  /// - Returns: A new URL with the appended query parameters.
+  ///   - If the URL or the provided parameters are invalid, the method returns `nil`.
+  ///
+  /// Example:
+  ///
+  /// ```swift
+  /// let baseURL = URL(string: "https://example.com")!
+  /// let parameters = ["key1": "value1", "key2": "value2"]
+  /// let resultURL = baseURL.appendingQueryParameters(parameters)
+  /// print(resultURL?.absoluteString) 
+  /// // Output: "https://example.com?key1=value1&key2=value2" or "https://example.com?key2=value2&key1=value1"
+  /// ```
+  ///
+  /// - Note: This method is designed to work with URLs that may or may not have existing query parameters.
+  ///   - If the URL already contains query parameters, the new parameters will be appended to the existing ones.
+  ///   - If the URL or parameters are invalid, the method returns `nil`.
+  ///
+  func appendingQueryParameters(_ parameters: [String: String]) -> URL? {
+    guard var components = URLComponents(url: self, resolvingAgainstBaseURL: true) else {
+      return nil
+    }
+
+    var queryItems = components.queryItems.or([])
+    queryItems += parameters.map { URLQueryItem(name: $0.key, value: $0.value) }
+    components.queryItems = queryItems
+
+    return components.url
+  }
+}
+
 extension URL: ExpressibleByStringLiteral {
   /// Creates a URL instance from a string literal.
   ///
--- Tests/SwiftPressTests/Foundation/URLTests.swift
+++ Tests/SwiftPressTests/Foundation/URLTests.swift
@@ -14,6 +14,37 @@ final class URLTests: XCTestCase {
 
   // MARK: - Tests
 
+  // MARK: - appendingQueryParameters(_:)
+
+  func testAppendingQueryParameters_GivenValidURLAndParameters_ThenShouldReturnNewURLWithAppendedParameters() {
+    let baseURL = URL(string: "https://example.com")!
+    let parameters = ["key1": "value1", "key2": "value2"]
+
+    let resultURL = baseURL.appendingQueryParameters(parameters)
+
+    XCTAssertTrue((resultURL?.absoluteString.contains("key2")).or(false), "The URL should have the appended query parameters.")
+  }
+
+  func testAppendingQueryParameters_GivenURLWithExistingParameters_ThenShouldReturnNewURLWithMergedParameters() {
+    let baseURL = URL(string: "https://example.com?key1=value1")!
+    let parameters = ["key2": "value2", "key3": "value3"]
+
+    let resultURL = baseURL.appendingQueryParameters(parameters)
+
+    XCTAssertTrue((resultURL?.absoluteString.contains("key3")).or(false), "The URL should have the merged query parameters.")
+  }
+
+  func testAppendingQueryParameters_GivenEmptyParameters_ThenShouldReturnSameURL() {
+    let baseURL = URL(string: "https://example.com")!
+    let emptyParameters: [String: String] = [:]
+
+    let resultURL = baseURL.appendingQueryParameters(emptyParameters)
+
+    XCTAssertTrue(!(resultURL?.absoluteString.contains("key1")).or(false), "Appending empty parameters should result in the same URL.")
+  }
+
+  // MARK: - init(stringLiteral:)
+
   func test_whenValidateValidURL_shouldNotBeNil() {
     // Given, When
     let appleURL: URL? = "https://www.apple.com"
